d214288e1b9098b98a7790d5e4871f82e50702f5
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-07-24 08:45:27 -0400

    Adds destructable blocks that you can hit with your melee attack

--- Assets/Scripts/MeleeHit.cs
+++ Assets/Scripts/MeleeHit.cs
@@ -1,14 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
 public class MeleeHit : GameplayPausable {
+	public LayerMask thignsICanHit;
 	public float lifespan = 0.75f;
 	private float duration = 0f;
-	// Update is called once per frame
+
 	public override void UnpausedUpdate () {
 		duration += Time.deltaTime;
 		if (duration > lifespan) {
 			Destroy(gameObject);
 		}
+	}
+
+	public void OnTriggerEnter2D(Collider2D other) {
+		other.SendMessage("MeleeHit", SendMessageOptions.DontRequireReceiver);
 	}
 }
