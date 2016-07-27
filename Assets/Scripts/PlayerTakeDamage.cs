6556e2e688f262e4e7a74e23035b39242e58a0d1
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-07-26 22:42:41 -0400

    Adds player health.
    Also tweaks knockback params.

--- Assets/Scripts/PlayerTakeDamage.cs
+++ Assets/Scripts/PlayerTakeDamage.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 [RequireComponent(typeof(PlayerMovement))]
 public class PlayerTakeDamage : MonoBehaviour {	
+	public int currentHealth;
+	public int maxHealth = 5;
+
 	public float iframesDurations = 1f;
 	public bool currentlyInIframes = false;
 
+	public void Start() {
+		currentHealth = maxHealth;
+	}
+
 	public void GetHit(int damage = 1) {
 		if (!currentlyInIframes) {
+			currentHealth -= damage;
 			StartCoroutine(enableIFrames(iframesDurations));
 			GetComponent<PlayerMovement>().KnockBack();
 		}
