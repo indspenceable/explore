172f838769dc227567c0fdc52fcb3eaed7b2c93a
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-07-30 16:58:50 -0400

    Adds a health indicator for the player.

---
+++ Assets/PlayerUI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(PlayerTakeDamage))]
+public class PlayerUI : MonoBehaviour {
+	private PlayerTakeDamage health;
+	public Text playerHealthText;
+
+	public void Start() {
+		health = GetComponent<PlayerTakeDamage>();
+	}
+
+	public void Update() {
+		playerHealthText.text = "Health: " + health.currentHealth;
+	}
+}
