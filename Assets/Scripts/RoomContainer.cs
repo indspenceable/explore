ded9231ad27823e5c2b0d87dede4d41d53836a26
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-08-01 20:27:05 -0400

    Adds minimum distance on camera, so it's less annoying

--- Assets/Scripts/RoomContainer.cs
+++ Assets/Scripts/RoomContainer.cs
@@ -9,9 +9,11 @@ public class RoomContainer : GameplayPausable {
 	public float lerpWeight = 5f;
 	public float dist = 1f;
 	public GameObject pausedTextContainer;
+	public float minDistanceThreshold = 0.02f;
 
 	void Start () {
 		GetComponent<AudioSource>().Play();
+		GoToPlayer();
 	}
 
 	public override void Update() {
@@ -29,11 +31,18 @@ public class RoomContainer : GameplayPausable {
 		}
 	}
 
+	public void GoToPlayer() {
+		transform.position = new Vector3(player.transform.position.x, player.transform.position.y, myCamera.transform.position.z);
+	}
+
 	// Update is called once per frame
 	public override void UnpausedUpdate () {
 		float px = player.transform.position.x + (player.facingLeft ? -dist : dist);
 		float py = player.transform.position.y;
 		// Can the camera go there?
 		myCamera.transform.position = Vector3.Lerp(myCamera.transform.position, new Vector3(px, py, myCamera.transform.position.z), Time.deltaTime * lerpWeight);
+		if (Mathf.Abs(myCamera.transform.position.x - player.transform.position.x) + Mathf.Abs(myCamera.transform.position.y - player.transform.position.y) < minDistanceThreshold) {
+			GoToPlayer();
+		}
 	}
 }
