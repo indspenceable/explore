55cd8e6c6d233aad4038bc0b04b76848e2f0d395
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-08-03 00:31:53 -0400

    Missiles correctly collide with walls.

--- Assets/Scripts/Player/MissileHit.cs
+++ Assets/Scripts/Player/MissileHit.cs
@@ -5,12 +5,18 @@ public class MissileHit : MonoBehaviour {
 	public float lifespan = 0.75f;
 	private float dt = 0f;
 	public Vector3 direction;
+	public LayerMask levelGeometryLayerMask;
+	private Collider2D myCollider;
+
+	public void Start() {
+		myCollider = GetComponent<Collider2D>();
+	}
 
 	public void Update () {
 		transform.position += direction*Time.deltaTime;
 
 		dt += Time.deltaTime;
-		if (dt > lifespan) {
+		if (dt > lifespan || Physics2D.IsTouchingLayers(myCollider, levelGeometryLayerMask)) {
 			Destroy(gameObject);
 		}
 	}
