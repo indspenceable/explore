8b33af1f797624e02b6496f29c48dd09c5fadf92
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-08-03 08:48:35 -0400

    Adds AirVents, similar to bubbles

---
+++ Assets/Scripts/LevelLayout/AirVent.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AirVent : MonoBehaviour {
+	public bool changeYVelocity;
+	public float yVelocityFactor;
+	public bool changeXVelocity;
+	public float xVelocityFactor;
+
+	public void OnTriggerStay2D(Collider2D other) {
+		// other should be player
+		PlayerMovement player = other.GetComponent<PlayerMovement>();
+		if (player != null && !player.disabled) {
+			if (changeYVelocity) {
+				player.vert.vy += (player.gravityStrength + yVelocityFactor) * Time.deltaTime;
+				player.initiatedJump = false;
+			}
+			if (changeXVelocity) {
+				player.horiz.vx += xVelocityFactor * Time.deltaTime;
+			}
+		}
+	}
+}
