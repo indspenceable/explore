8ffea04afbc56feed18c38a2f0b100504c772b41
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-08-02 20:29:44 -0400

    Adds a crawler enemy

---
+++ Assets/Crawler.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crawler : MonoBehaviour {
+	public Transform rightGround;
+	public Transform rightWall;
+	public Transform leftGround;
+	public Transform leftWall;
+	public LayerMask walls;
+
+	public float crawlPeriod = 1f;
+	public float crawlVelocityScale = 1f;
+	public AnimationCurve crawlVelocity;
+	public bool facingRight = true;
+
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(Walk());
+	}
+
+	private bool checkCurrentDirection() {
+		if (facingRight) {
+			return Physics2D.OverlapPoint(rightGround.position, walls) && !Physics2D.OverlapPoint(rightWall.position, walls);
+		} else {
+			return Physics2D.OverlapPoint(leftGround.position, walls) && !Physics2D.OverlapPoint(leftWall.position, walls);
+		}
+	}
+
+	public int scaleByDirection() {
+		return facingRight ? 1 : -1;
+	}
+
+	private IEnumerator Walk() {
+		float dt = 0f;
+		while (dt < crawlPeriod) {
+			yield return null;
+			dt += Time.deltaTime;
+			float pct = dt/crawlPeriod;
+			transform.Translate(Vector3.right * crawlVelocity.Evaluate(pct) * Time.deltaTime * crawlVelocityScale * scaleByDirection(), Space.World);
+			if (!checkCurrentDirection()) {
+				facingRight = !facingRight;
+				GetComponent<SpriteRenderer>().flipX = !facingRight;
+				break;
+			}
+		}
+		yield return StartCoroutine(Walk());
+	}
+}
