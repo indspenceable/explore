eebc6783100524087015e4365a9366cf6168e4e2
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-08-11 19:20:43 -0400

    more sprite work.

--- Assets/DoorCollider.cs
+++ Assets/DoorCollider.cs
@@ -5,8 +5,9 @@ public class DoorCollider : MonoBehaviour {
 	public GameManager.Direction direction;
 
 	public void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.layer == LayerMask.NameToLayer("Player") ||
-			other.transform.parent.gameObject.layer == LayerMask.NameToLayer("Player")) {
+		if (other && other.gameObject.layer == LayerMask.NameToLayer("Player") ||
+		   (other.transform.parent && 
+				other.transform.parent.gameObject.layer == LayerMask.NameToLayer("Player"))) {
 			GameManager gm = GameManager.instance;
 			gm.StartCoroutine(gm.DoorCollision(this));
 		}
