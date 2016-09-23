c53499fd6c72f643211d1e7f60d075b59ebd37ae
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-09-23 14:32:43 -0400

    Adds background to FPS display

--- Assets/Scripts/Map/FPS.cs
+++ Assets/Scripts/Map/FPS.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class FPS : MonoBehaviour
 {
 	float deltaTime = 0.0f;
+	Texture2D backgroundColor;
 
 	void Update()
 	{
@@ -13,6 +14,10 @@ public class FPS : MonoBehaviour
 	void OnGUI()
 	{
 		int w = Screen.width, h = Screen.height;
+		if (backgroundColor == null) {
+			backgroundColor = new Texture2D (1, 1);
+			backgroundColor.SetPixel (0, 0, Color.white);
+		}
 
 		GUIStyle style = new GUIStyle();
 
@@ -20,6 +25,8 @@ public class FPS : MonoBehaviour
 		style.alignment = TextAnchor.UpperLeft;
 		style.fontSize = h * 2 / 100;
 		style.normal.textColor = new Color (0.0f, 0.0f, 0.5f, 1.0f);
+		style.normal.background = backgroundColor;
+
 		float msec = deltaTime * 1000.0f;
 		float fps = 1.0f / deltaTime;
 		string text = string.Format("{0:0.0} ms ({1:0.} fps)", msec, fps);
