5ec0e30824dd924e3fef153b2f21ae5e90eaa795
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2016-10-02 10:53:10 -0400

    Custom 'upgrade acquired' messages per-powerup

--- Assets/Scripts/Powerup.cs
+++ Assets/Scripts/Powerup.cs
@@ -1,8 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Powerup : MonoBehaviour, IActivatableObject {
 	public GameStateFlag upgrade;
+	public static Dictionary<GameStateFlag, string> upgradeMessages = new Dictionary<GameStateFlag, string>() {
+		{GameStateFlag.MELEE, "Acquired Melee Attack.\n\nHit <c> to use."},
+		{GameStateFlag.HIGH_JUMP, "Acquired High Jump."},
+		{GameStateFlag.DOUBLE_JUMP, "Acquried Double Jump.\n\nHit <z> while airborn to use."},
+		{GameStateFlag.SHADOW_STEP, "Acquried Shadowstep.\n\nHit <v>+a direction to use."},
+	};
+
+
+
 	public void Activate() {
 		if (GameManager.instance.player.currentGameState.enabled(upgrade)) {
 			Destroy(gameObject);
@@ -12,7 +22,8 @@ public class Powerup : MonoBehaviour, IActivatableObject {
 	void OnTriggerEnter2D(Collider2D other) {
 		if (other.gameObject.layer == LayerMask.NameToLayer("Player")) {
 			GameManager.instance.player.currentGameState.enable(upgrade);
-			GameManager.instance.StartCoroutine (GameManager.instance.Read ("Got Powerup: " + upgrade.ToString(), null));
+//			GameManager.instance.StartCoroutine (GameManager.instance.Read ("Got Powerup: " + upgrade.ToString(), null));
+			GameManager.instance.StartCoroutine(GameManager.instance.Read(upgradeMessages[upgrade], null));
 			Destroy(gameObject);
 		}
 	}
