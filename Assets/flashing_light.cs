fecb09a6b3d429cacac3735e44aa978e8e98f8d1
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2017-02-24 15:56:45 -0500

    Adds flashing lights in level 3

---
+++ Assets/flashing_light.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashing_light : MonoBehaviour {
+	private SpriteRenderer sr;
+	// Use this for initialization
+	void Start () {
+		this.sr = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float t = Time.time;
+		t /= 10;
+		t -= (int)t;
+		this.sr.color = Color.HSVToRGB(t, 1f, 1f);
+	}
+}
