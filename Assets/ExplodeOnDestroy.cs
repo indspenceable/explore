21fa0e3135919415279a052272361732987ec2c5
Daniel Spencer <daniel.patrick.spencer@gmail.com>
2017-03-03 14:57:45 -0500

    Adds explosion particles to standard d enemies

---
+++ Assets/ExplodeOnDestroy.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExplodeOnDestroy : MonoBehaviour {
+	public GameObject ExplosionPrefab;
+	public void OnDestroy() {
+		Instantiate(ExplosionPrefab, transform.position, Quaternion.identity, GameManager.instance.currentActiveObjects.transform);
+	}
+}
